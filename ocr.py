b27b30af39fa1d7d14dfa26aeadc9fbade5f495d
CD <dch@dchair.local>
2022-04-01 17:03:38 -0500

    add output

--- ocr.py
+++ ocr.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "-i", "--input", type=str, nargs='+', required='True', help='input file(s) to operate on')
 parser.add_argument(
-    "-o", "--output", type=str, nargs='+', help='output file for converted text')
+    "-o", "--output", type=str, required='True', help='output file for converted text')
 args = parser.parse_args()
 
 def pdf_to_image(pdf):
@@ -21,27 +21,29 @@ def pdf_to_image(pdf):
     count = 1
     images = []
     for page in doc:
-        image_name = f"{pdf}"+"page_"+str(count)+".jpg"
+        image_name = f"{pdf}"+"_page_"+str(count)+".jpg"
         page.save(image_name, 'JPEG')
         images.append(image_name)
         count += 1
-    print(images)
     return images
 
 def convert_to_text(image):
     """Use OCR to scrape text from image"""
     text = pytesseract.image_to_string(Image.open(image))
     print(text)
+    return text
+
+def write_out(text):
+    out_file = args.output
+    with open(out_file, 'w') as out:
+        out.write(text)
 
 for arg in args.input:
     if arg.endswith('.pdf'):
         pdf_images = pdf_to_image(arg)
         for image in pdf_images:
-            convert_to_text(image)
+            text = convert_to_text(image)
+            write_out(text)
     else:
-        convert_to_text(arg)
-
-
-
-
-
+        text = convert_to_text(arg)
+        write_out(text)
